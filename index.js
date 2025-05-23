d35431d51b1e0a3621dc3e3b1e8e08b35582bc70
vishak45 <vishak.r565@gmail.com>
2025-05-23 22:15:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const blogRoutes = require("./routes/blogRoutes");
 const app = express();
 const path = require("path");
 dotenv.config();
-port=3000;
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
 connectDB();
